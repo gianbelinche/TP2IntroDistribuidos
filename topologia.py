06901b046ae2964e324ca134fa2edecad72f6476
None <None>
2021-12-18 14:14:13 -0800

    Agrego parametro  a la topologia

--- topologia.py
+++ topologia.py
@@ -1,27 +1,27 @@
 from mininet.topo import Topo
 
 class Topologia(Topo):
-    def __init__(self,n=2):
+    def build(self, switches=2):
+        
+        switches = max(switches, 2)
 
-        Topo.__init__(self)
-
-        switches = []
-
-        for i in range(0,n):
-            switches.append(self.addSwitch("switch_{}".format(i)))
-
+        swtches = []
+        
+        for i in range(0,switches):
+            swtches.append(self.addSwitch("switch_{}".format(i)))
+    
         h1 = self.addHost("host_1")
         h2 = self.addHost("host_2")
         h3 = self.addHost("host_3")
         h4 = self.addHost("host_4")
 
-        self.addLink(h1,switches[0])
-        self.addLink(h2,switches[0])
-        self.addLink(h3,switches[-1])
-        self.addLink(h4,switches[-1])
+        self.addLink(h1,swtches[0])
+        self.addLink(h2,swtches[0])
+        self.addLink(h3,swtches[-1])
+        self.addLink(h4,swtches[-1])
 
-        for i in range(0,n-1):
-            self.addLink(switches[i],switches[i+1])
+        for i in range(0,switches-1):
+            self.addLink(swtches[i],swtches[i+1])
 
 
 
