a4dbfbf672af0aa140f181f2f957e7b1ff2a3ae0
Gianbelinche <gbelinche@fi.uba.ar>
2021-12-20 13:15:06 -0300

    Añado topo ejemplo

--- controlador.py
+++ controlador.py
@@ -25,43 +25,43 @@ class Firewall(EventMixin):
 
     def regla_1(self,event):
         #Todo lo que vaya al puerto 80 debe descartarse
-        block = of.ofp_match()
-        block.dl_type=IP
-        block.nw_proto=TCP 
-        block.tp_dst = 80 #Para poder seleccionar que el puerto sea el 80, deben si o si especificarse las capas anteriores, es decir, IP y TCP
-        flow_mod = of.ofp_flow_mod()
-        flow_mod.match = block
-        event.connection.send(flow_mod)
+        bloque = of.ofp_match()
+        bloque.dl_type=IP
+        bloque.nw_proto=TCP 
+        bloque.tp_dst = 80 #Para poder seleccionar que el puerto sea el 80, deben si o si especificarse las capas anteriores, es decir, IP y TCP
+        openflow_packet = of.ofp_flow_mod()
+        openflow_packet.match = bloque
+        event.connection.send(openflow_packet)
 
-        block = of.ofp_match()
-        block.dl_type=IP
-        block.nw_proto=UDP
-        block.tp_dst = 80
-        flow_mod = of.ofp_flow_mod()
-        flow_mod.match = block
-        event.connection.send(flow_mod)
+        bloque = of.ofp_match()
+        bloque.dl_type=IP
+        bloque.nw_proto=UDP
+        bloque.tp_dst = 80
+        openflow_packet = of.ofp_flow_mod()
+        openflow_packet.match = bloque
+        event.connection.send(openflow_packet)
         log.debug("Firewall rules 1 installed on %s", dpidToStr(event.dpid))
 
     def regla_2(self,event):
         #Mensajes provenientes del host 1, UDP, puerto destino 5001 se descartan
-        block = of.ofp_match()
-        block.dl_type=IP
-        block.nw_proto=UDP
-        block.tp_dst = 5001
-        block.dl_src = EthAddr('00:00:00:00:00:01')
-        flow_mod = of.ofp_flow_mod()
-        flow_mod.match = block
-        event.connection.send(flow_mod)
+        bloque = of.ofp_match()
+        bloque.dl_type=IP
+        bloque.nw_proto=UDP
+        bloque.tp_dst = 5001
+        bloque.dl_src = EthAddr('00:00:00:00:00:01')
+        openflow_packet = of.ofp_flow_mod()
+        openflow_packet.match = bloque
+        event.connection.send(openflow_packet)
         log.debug("Firewall rules 1 installed on %s", dpidToStr(event.dpid))
 
     def regla_3(self,event):
         #Los hosts 1 y 2 no pueden comunicarse
-        block = of.ofp_match()
-        block.dl_src = EthAddr('00:00:00:00:00:01')
-        block.dl_dst = EthAddr('00:00:00:00:00:02')
-        flow_mod = of.ofp_flow_mod()
-        flow_mod.match = block
-        event.connection.send(flow_mod)
+        bloque = of.ofp_match()
+        bloque.dl_src = EthAddr('00:00:00:00:00:01')
+        bloque.dl_dst = EthAddr('00:00:00:00:00:02')
+        openflow_packet = of.ofp_flow_mod()
+        openflow_packet.match = bloque
+        event.connection.send(openflow_packet)
         log.debug("Firewall rules 3 installed on %s", dpidToStr(event.dpid))
 
 def launch():
--- topologia.py
+++ topologia.py
@@ -3,7 +3,7 @@ from mininet.topo import Topo
 class Topologia(Topo):
     def build(self, switches=2):
         
-        switches = max(switches, 2)
+        switches = max(switches, 1)
 
         swtches = []
         
@@ -24,5 +24,24 @@ class Topologia(Topo):
             self.addLink(swtches[i],swtches[i+1])
 
 
+class Ejemplo(Topo):
+    def __init__(self):
+        # Initialize topology
+        Topo.__init__(self)
 
-topos = { "tp2": Topologia }
+        # Create switch
+        s1 = self.addSwitch("switch_1")
+        s2 = self.addSwitch("switch_2")
+
+        # Create hosts
+        h1 = self.addHost("host_1")
+        h2 = self.addHost("host_2")
+        h3 = self.addHost("host_3")
+
+        # Add links between switches and hosts self.addLink(s1, s2)
+        self.addLink(s1, h1)
+        self.addLink(s1, h2)
+        self.addLink(s2, h3)
+
+
+topos = { "tp2": Topologia ,"ejemplo" : Ejemplo}
